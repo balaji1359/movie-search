3f9701caebc6e0d8bc73b1bd70b20a8c6b5e1473
Balaji <nagisetty.balaji@gmail.com>
2018-10-14 09:32:09 +0200

    Updates

---
+++ precache-manifest.9bd1bd4b1ea6dcbeebabd5f68784bc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05c144d15085a2fa07d3",
+    "url": "/movie-search/static/js/runtime~main.05c144d1.js"
+  },
+  {
+    "revision": "4040ab0484efaa4ec7c0",
+    "url": "/movie-search/static/js/main.4040ab04.chunk.js"
+  },
+  {
+    "revision": "767c3583f6f155fbd6c7",
+    "url": "/movie-search/static/js/1.767c3583.chunk.js"
+  },
+  {
+    "revision": "4040ab0484efaa4ec7c0",
+    "url": "/movie-search/static/css/main.54ea393d.chunk.css"
+  },
+  {
+    "revision": "60988e8fd88df64bd12808cd90de2220",
+    "url": "/movie-search/index.html"
+  }
+];
